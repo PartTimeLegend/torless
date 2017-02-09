1581102d9df18afd3be00347d301d6132f0c7c04
Antony Bailey <antony.bailey@GTS.local>
2017-02-09 11:11:12 +0000

    TorlessCSharp

---
+++ TorlessCSharp/TorlessCSharp/TorlessCSharp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+
+namespace TorlessCSharp
+{
+    public static class TorlessCSharp
+    {
+        public static bool IsATorExitNode(string clientIpAddress, string serverIpAddress, int portToCheck = 80)
+        {
+            string addressToCheck = ReverseGraphemeClusters(clientIpAddress) + "." + portToCheck + "." + ReverseGraphemeClusters(serverIpAddress) + ".ip-port.exitlist.torproject.org";
+            IPHostEntry ipHostEntry = Dns.GetHostEntry(addressToCheck);
+
+            return ipHostEntry.AddressList[0].ToString() == "127.0.0.2";
+        }
+        private static IEnumerable<string> GraphemeClusters(this string s)
+        {
+            TextElementEnumerator enumerator = StringInfo.GetTextElementEnumerator(s);
+            while (enumerator.MoveNext())
+            {
+                yield return (string)enumerator.Current;
+            }
+        }
+        private static string ReverseGraphemeClusters(string s)
+        {
+            return string.Join("", s.GraphemeClusters().Reverse().ToArray());
+        }
+
+    }
+}
